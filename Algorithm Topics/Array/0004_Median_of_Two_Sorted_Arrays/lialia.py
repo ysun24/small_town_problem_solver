68021d1e840f4879f830a69a97bb57a092ace5df
ysun24 <59858271+ysun24@users.noreply.github.com>
2021-12-20 10:30:37 -0800

    [0004]lialia

--- Algorithm Topics/Array/0004_Median_of_Two_Sorted_Arrays/lialia.py
+++ Algorithm Topics/Array/0004_Median_of_Two_Sorted_Arrays/lialia.py
@@ -1,10 +1,38 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        disc={}
-        index=0
-        for i in nums:
-            if (target-i) not in disc.keys():
-                disc[i]=index
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        index1=0
+        index2=0
+        l=(len(nums1)+len(nums2)) / 2
+        flag=(len(nums1)+len(nums2)) % 2
+        
+        if(len(nums1)>index1):
+            num1=nums1[index1]
+        else:
+            num1=float('inf')
+            
+        if(len(nums2)>index2):
+            num2=nums2[index2]
+        else:
+            num2=float('inf')
+        
+        tempList=[]
+        while len(tempList)<=l:
+            if num1<num2:
+                tempList.append(num1)
+                index1+=1
+                if(len(nums1)>index1):
+                    num1=nums1[index1]
+                else:
+                    num1=float('inf')
             else:
-                return [disc[target-i],index]
-            index+=1
+                tempList.append(num2)
+                index2+=1
+                if(len(nums2)>index2):
+                    num2=nums2[index2]
+                else:
+                    num2=float('inf')
+                    
+        if flag==1:
+            return tempList[-1]
+        else:
+            return (tempList[-1]+tempList[-2])/2.0
