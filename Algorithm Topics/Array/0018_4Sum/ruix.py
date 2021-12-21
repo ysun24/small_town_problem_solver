d19ccd2c3ff1879e9c6617ca1339f32d3d745e34
Rui-Wang-813 <ruiwang813@berkeley.edu>
2021-12-20 17:46:06 -0800

    [0018] 4 Sum RUIX

---
+++ Algorithm Topics/Array/0018_4Sum/ruix.py
@@ -0,0 +1,53 @@
+from typing import List
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        i = 0
+        nums.sort()
+        while i < len(nums) - 3:
+            _nums = nums[i+1:]
+            _res = self.threeSum(_nums, target - nums[i])
+            for _r in _res:
+                res.append([nums[i]] + _r)
+            
+            l = i + 1
+            while (l < len(nums) and nums[l] == nums[i]):
+                l += 1
+            i = l
+        
+        return res
+    
+    def threeSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        i = 0
+        while i < len(nums):
+            _sum = target - nums[i]
+            
+            j = i + 1
+            k = len(nums)-1
+            while (j < k):
+                if nums[j] + nums[k] == _sum:
+                    res.append([nums[i], nums[j], nums[k]])
+                    while (j < k and nums[j+1] == nums[j]):
+                        j += 1
+                    while (k > j and nums[k-1] == nums[k]):
+                        k -= 1
+                    j, k = j+1, k-1
+                elif (nums[j] + nums[k] < _sum):
+                    j += 1
+                else:
+                    k -= 1
+
+            l = i + 1
+            while (l < len(nums) and nums[i] == nums[l]):
+                l += 1
+            i = l
+                    
+        
+        return res
+
+sol = Solution()
+print(sol.fourSum([2] * 5, 8))
