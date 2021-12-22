9c397d3de2090dff0a74f4bfa7aec2c6fecd3ddc
Rui-Wang-813 <ruiwang813@berkeley.edu>
2021-12-21 22:21:52 -0800

    [0045][ver 2] Jump Game II --More efficient RUIX

--- Algorithm Topics/Array/0045_Jump_Game_II/ruix.py
+++ Algorithm Topics/Array/0045_Jump_Game_II/ruix.py
@@ -2,16 +2,38 @@ from typing import List
 
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        idx_to_steps = [float('inf')] * len(nums)
-        idx_to_steps[len(nums)-1] = [0]
+        _n = len(nums)
 
-        i = len(nums) - 2
-        while i >= 0:
-            if nums[i] >= len(nums)-i-1:
-                idx_to_steps[i] = 1
-            else:
-                for n_steps in range(1, nums[i]+1):
-                    idx_to_steps[i] = min(idx_to_steps[i+n_steps]+1, idx_to_steps[i])
-            i -= 1
+        if _n <= 1:
+            return 0
+        if nums[0] >= _n-1:
+            return 1
         
-        return idx_to_steps[0]
+        current_pos, checked_pos = 0, -1
+        jump_to = current_pos + nums[current_pos]
+        n_steps = 1
+
+        while jump_to < _n-1:
+            current_pos = self.findNextPos(current_pos, checked_pos, nums)
+            checked_pos = jump_to
+            jump_to = current_pos + nums[current_pos]
+            n_steps += 1
+        
+        return n_steps
+
+    def findNextPos(self, current_pos: int, checked_pos: int, nums: List[int]) -> int:
+        # By the iteration conditions of the above function, each time this function
+        # is called, checked_pos < len(nums)-1.
+        new_pos = checked_pos+1
+        i = checked_pos+1
+
+        while i <= current_pos + nums[current_pos]:
+            if i+nums[i] > new_pos+nums[new_pos]:
+                new_pos = i
+            i += 1
+        
+        return new_pos
+
+'''
+Easily proved by contradiction that the above function gives optimal solution.
+'''
