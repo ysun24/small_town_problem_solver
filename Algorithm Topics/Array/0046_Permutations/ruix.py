aef8b1b30bc6ec5d0e3031cc85885803154a142d
Rui-Wang-813 <ruiwang813@berkeley.edu>
2021-12-21 22:33:36 -0800

    [0046] Permutations RUIX

--- Algorithm Topics/Array/0046_Permutations/ruix.py
+++ Algorithm Topics/Array/0046_Permutations/ruix.py
@@ -1,1 +1,16 @@
 from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 1:
+            return [nums]
+        
+        res = []
+        for i in range(len(nums)):
+            _res = self.permute(nums[:i] + nums[i+1:])
+            res.extend([[nums[i]] + _r for _r in _res])
+        
+        return res
+
+sol = Solution()
+print(sol.permute([1,2,3]))
