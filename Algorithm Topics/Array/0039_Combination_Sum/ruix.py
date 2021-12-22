7e25493717aaf6c96da5b12dfea8957e806f2666
Rui-Wang-813 <ruiwang813@berkeley.edu>
2021-12-21 19:53:14 -0800

    [0039] Combination Sum RUIX

--- Algorithm Topics/Array/0039_Combination_Sum/ruix.py
+++ Algorithm Topics/Array/0039_Combination_Sum/ruix.py
@@ -1,1 +1,17 @@
 from typing import List
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+        candidates.sort()
+
+        def search(cand: List[int], stk: List[int], target: int):
+            for i in range(len(cand)):
+                if cand[i] == target:
+                    res.append(stk + [cand[i]])
+                elif cand[i] < target:
+                    search(cand[i:], stk + [cand[i]], target-cand[i])
+        
+        search(candidates, [], target)
+
+        return res
